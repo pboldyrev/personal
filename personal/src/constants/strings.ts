48121e2d2e33cb88a4cfd37fa4d89534010d9fed
Paul <impaulboldyrev@gmail.com>
2024-04-07 19:50:43 -0700

    Typo fix in CTA

--- personal/src/constants/strings.ts
+++ personal/src/constants/strings.ts
@@ -2,7 +2,7 @@ export const INTRODUCTION = {
   TITLE: "Pavel Boldyrev",
   SUBTITLE:
     "Fullstack engineer with an eye for user-centric and delightful design.",
-  PRIMARY_CTA: "Talk with me",
+  PRIMARY_CTA: "Talk to me",
   SECONDARY_CTA: "See my work",
   TAG: "Avaliable",
 };
