a4af27a6990bef78312f413276693f84e79cc474
Paul <impaulboldyrev@gmail.com>
2024-04-07 19:40:36 -0700

    Fixed gap

--- personal/src/components/App.js
+++ personal/src/components/App.js
@@ -5,7 +5,7 @@ import Introduction from "./Introduction";
 
 function App() {
   return (
-    <div className="row justify-content-center">
+    <div className="row justify-content-center gap-0">
       <div className="app-body mt-3 mt-sm-4 mt-md-5 px-3 px-sm-4 px-md-5 col-11 col-sm-9 col-md-8 col-lg-7">
         <Introduction />
       </div>
