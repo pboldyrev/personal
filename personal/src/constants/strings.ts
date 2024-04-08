4ebb82b3529543c4ccbfb1a317ca43af882bd9df
Paul <impaulboldyrev@gmail.com>
2024-04-07 20:15:00 -0700

    Fixed CTA

--- personal/src/constants/strings.ts
+++ personal/src/constants/strings.ts
@@ -2,7 +2,7 @@ export const INTRODUCTION = {
   TITLE: "Pavel Boldyrev",
   SUBTITLE:
     "Fullstack engineer with an eye for user-centric and delightful design.",
-  PRIMARY_CTA: "Talk to me",
+  PRIMARY_CTA: "Schedule a chat",
   SECONDARY_CTA: "See my work",
   TAG: "Avaliable",
 };
